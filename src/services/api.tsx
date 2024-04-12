3d0657d40cf0f25c74c860b834c09ebda8b0a8eb
WillianM19 <willianm1928@gmail.com>
2024-04-12 20:58:41 -0300

    ✨ Modal de notificação e consumo

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,1 +1,12 @@
 //Requisições aqui
+
+import { apiRequest } from "../utils/axios";
+
+export async function getNotificationsList() {
+    try {
+        const res = await apiRequest.get('/notifications')
+        return res.data
+    } catch(_) {
+        return []
+    }
+}
